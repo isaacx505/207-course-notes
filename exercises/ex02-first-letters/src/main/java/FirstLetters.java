32b93dc01d9b3346a79e3d11ebfa18931efb3b5c
antibiotics505 <sbren114@gmail.com>
2026-09-13 08:28:14 -0400

    Implement first letters for ex02

--- exercises/ex02-first-letters/src/main/java/FirstLetters.java
+++ exercises/ex02-first-letters/src/main/java/FirstLetters.java
@@ -26,6 +26,11 @@ public class FirstLetters {
      */
     public static String firstLetters(String words) {
         // TODO: complete
-        return "";
+        StringBuilder result = new StringBuilder();
+        String[] wordArray = words.split(" ");
+        for (String word : wordArray) {
+            result.append(word.charAt(0));
+        }
+        return result.toString();
     }
 }
